8099cdaa4c2671e01829549806eb276f03cf41b6
21127720 <dvtuong21@clc.fitus.edu.vn>
2025-03-05 21:23:55 +0700

    FE: Ready. Set. Go!!!

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{"align-content": "center", "height": "100vh", "width": "100vw", "background-color": "#181818", "color": "#898989", "text-align": "center", "font-size": "2em", "font-weight": "bolder"}}>
+      Hello world
+    </div>
+  )
+}
+
+export default App
